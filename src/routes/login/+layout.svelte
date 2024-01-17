de1970d256e98641333385eb4e0b9ef7528aa4fe
Caleb Young <caleb.a.young@gmail.com>
2024-01-17 15:40:17 -0700

    Implement login, add FontAwesome

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -10,11 +10,9 @@ import AnimatedRoute from '$lib/components/AnimatedRoute.svelte'
         <li class="md:flex-1">
           <a
             href="/login"
-            class="group flex flex-col border-l-4 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4 border-indigo-600 hover:border-indigo-800"
+            class="group flex flex-col border-l-4 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4 border-lime-600 hover:border-lime-800"
           >
-            <span
-              class="text-sm font-medium text-indigo-600 group-hover:text-indigo-800"
-            >
+            <span class="text-sm font-medium text-lime-600 group-hover:text-lime-800">
               Step 1
             </span>
             <span class="text-sm font-medium">Sign in</span>
@@ -26,14 +24,14 @@ import AnimatedRoute from '$lib/components/AnimatedRoute.svelte'
             class="group flex flex-col border-l-4 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
             class:border-gray-200={!$page.route.id?.match(/username|photo/g)}
             class:hover:border-gray-300={!$page.route.id?.match(/username|photo/g)}
-            class:border-indigo-600={$page.route.id?.match(/username|photo/g)}
+            class:border-lime-600={$page.route.id?.match(/username|photo/g)}
           >
             <span
-              class="text-sm font-medium text-indigo-600"
+              class="text-sm font-medium text-lime-600"
               class:text-gray-500={!$page.route.id?.match(/username|photo/g)}
               class:group-hover:text-gray-700={!$page.route.id?.match(/username|photo/g)}
-              class:text-indigo-600={$page.route.id?.match(/username|photo/g)}
-              class:group-hover:text-indigo-800={$page.route.id?.match(/username|photo/g)}
+              class:text-lime-600={$page.route.id?.match(/username|photo/g)}
+              class:group-hover:text-lime-800={$page.route.id?.match(/username|photo/g)}
             >
               Step 2
             </span>
@@ -46,14 +44,14 @@ import AnimatedRoute from '$lib/components/AnimatedRoute.svelte'
             class="group flex flex-col border-l-4 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
             class:border-gray-200={!$page.route.id?.match(/photo/g)}
             class:hover:border-gray-300={!$page.route.id?.match(/photo/g)}
-            class:border-indigo-600={$page.route.id?.match(/photo/g)}
+            class:border-lime-600={$page.route.id?.match(/photo/g)}
           >
             <span
               class="text-sm font-medium text-gray-500 group-hover:text-gray-700"
               class:text-gray-500={!$page.route.id?.match(/photo/g)}
               class:group-hover:text-gray-700={!$page.route.id?.match(/photo/g)}
-              class:text-indigo-600={$page.route.id?.match(/photo/g)}
-              class:group-hover:text-indigo-800={$page.route.id?.match(/photo/g)}
+              class:text-lime-600={$page.route.id?.match(/photo/g)}
+              class:group-hover:text-lime-800={$page.route.id?.match(/photo/g)}
             >
               Step 3
             </span>
@@ -64,7 +62,7 @@ import AnimatedRoute from '$lib/components/AnimatedRoute.svelte'
     </nav>
     <AnimatedRoute>
       <div class="bg-white shadow sm:rounded-lg">
-        <div class="px-4 py-5 sm:p-6">
+        <div class="px-4 py-10">
           <slot />
         </div>
       </div>
