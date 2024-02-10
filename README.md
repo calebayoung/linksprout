d44bd58178dcc107385588dc92dc52a7d4d746a4
Caleb Young <caleb.a.young@gmail.com>
2024-02-10 15:13:31 -0700

    Update README, hide username fields if username has been set

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-![ESLint](https://github.com/calebayoung/linksprout/actions/workflows/main.yml/badge.svg)
+# 🌱 LinkSprout
 
-# create-svelte
+![ESLint Pipeline Status](https://github.com/calebayoung/linksprout/actions/workflows/main.yml/badge.svg)
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/main/packages/create-svelte).
+A basic link-sharing application, modeled after LinkTree.
 
-## Creating a project
+Built following the [Fireship SvelteKit Full Course](https://fireship.io/courses/sveltekit/)
 
-If you're seeing this, you've probably already done this step. Congrats!
+### Important Commands
 
+Install dependencies:
 ```bash
-# create a new project in the current directory
-npm create svelte@latest
-
-# create a new project in my-app
-npm create svelte@latest my-app
+npm install
 ```
 
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
+Run locally:
 ```bash
 npm run dev
 
@@ -27,14 +21,12 @@ npm run dev
 npm run dev -- --open
 ```
 
-## Building
-
-To create a production version of your app:
-
+Build a production version of the app:
 ```bash
 npm run build
 ```
 
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+Preview a production version of the app:
+```bash
+npm run preview
+```
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { doc, getDoc, writeBatch } from 'firebase/firestore'
-import { db, user } from '$lib/firebase'
+import { db, user, userData } from '$lib/firebase'
 import AuthCheck from '$lib/components/AuthCheck.svelte'
 import LoginCardHead from '$lib/components/LoginCardHead.svelte'
 
@@ -49,51 +49,66 @@ async function confirmUsername (): Promise<void> {
 
 <AuthCheck>
   <LoginCardHead icon="fa-user" title="Choose your username"/>
-  <form on:submit|preventDefault={confirmUsername} class="max-w-96 mx-auto mt-6" autocomplete="off">
-    <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-    <div class="relative mt-2 rounded-md shadow-sm">
-      <input
-        type="text"
-        name="username"
-        id="username"
-        bind:value={username}
-        on:input={checkAvailability}
-        class="block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-lime-600"
-        class:text-red-900={availabilityChecked && !isAvailable && username.length > 0}
-        class:ring-red-300={availabilityChecked && !isAvailable && username.length > 0}
-        class:placeholder:text-red-300={availabilityChecked && !isAvailable && username.length > 0}
-        class:focus:ring-red-600={availabilityChecked && !isAvailable && username.length > 0}
-        placeholder="link-sprout"
-        aria-invalid="true"
-        aria-describedby="email-error"
-      >
-      {#if availabilityChecked && !isAvailable && username.length > 0}
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-          <i class="fa-regular fa-circle-exclamation text-red-500"></i>
-        </div>
-      {/if}
-      {#if loading}
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-          <i class="fa-regular fa-loader"></i>
-        </div>
-      {/if}
-      {#if availabilityChecked && isAvailable}
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-          <i class="fa-regular fa-circle-check text-lime-600"></i>
-        </div>
-      {/if}
+  {#if $userData?.username}
+    <div class="flex flex-col justify-center items-center mt-8 space-y-6">
+      <p class="text-center">Your username is <strong>@{ $userData.username }</strong></p>
+      <p class="text-center">(Usernames cannot be changed.)</p>
+      <a href="/login/photo">
+        <button
+          type="button"
+          class="rounded-md bg-lime-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
+        >
+          Upload profile image
+        </button>
+      </a>
     </div>
-    {#if availabilityChecked && !isAvailable && username.length > 0}
-      <p class="mt-2 text-sm text-red-600" id="email-error">This username is not available.</p>
-    {/if}
-    <button
-      type="submit"
-      class="rounded-md bg-lime-600 disabled:bg-gray-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 disabled:hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 disabled:focus-visible:outline-gray-300 mt-4"
-      disabled={!isAvailable}
-    >
-      Confirm username{username.length > 0 ? ' @' + username : ''}
-    </button>
-  </form>
+  {:else}
+    <form on:submit|preventDefault={confirmUsername} class="max-w-96 mx-auto mt-6" autocomplete="off">
+      <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+      <div class="relative mt-2 rounded-md shadow-sm">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          bind:value={username}
+          on:input={checkAvailability}
+          class="block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-lime-600"
+          class:text-red-900={availabilityChecked && !isAvailable && username.length > 0}
+          class:ring-red-300={availabilityChecked && !isAvailable && username.length > 0}
+          class:placeholder:text-red-300={availabilityChecked && !isAvailable && username.length > 0}
+          class:focus:ring-red-600={availabilityChecked && !isAvailable && username.length > 0}
+          placeholder="link-sprout"
+          aria-invalid="true"
+          aria-describedby="email-error"
+        >
+        {#if availabilityChecked && !isAvailable && username.length > 0}
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <i class="fa-regular fa-circle-exclamation text-red-500"></i>
+          </div>
+        {/if}
+        {#if loading}
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <i class="fa-regular fa-loader"></i>
+          </div>
+        {/if}
+        {#if availabilityChecked && isAvailable}
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <i class="fa-regular fa-circle-check text-lime-600"></i>
+          </div>
+        {/if}
+      </div>
+      {#if availabilityChecked && !isAvailable && username.length > 0}
+        <p class="mt-2 text-sm text-red-600" id="email-error">This username is not available.</p>
+      {/if}
+      <button
+        type="submit"
+        class="rounded-md bg-lime-600 disabled:bg-gray-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 disabled:hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600 disabled:focus-visible:outline-gray-300 mt-4"
+        disabled={!isAvailable}
+      >
+        Confirm username{username.length > 0 ? ' @' + username : ''}
+      </button>
+    </form>
+  {/if}
 </AuthCheck>
 
 <style>
