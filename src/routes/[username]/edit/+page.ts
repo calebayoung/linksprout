caecdea6bd98fa5e30d3feba394dda2a9a84956a
Caleb Young <cyoung@simplenexus.com>
2024-04-19 17:07:26 -0600

    Fix eslint error in CI/CD

--- src/routes/[username]/edit/+page.ts
+++ src/routes/[username]/edit/+page.ts
@@ -2,5 +2,5 @@ import type { PageLoad } from './$types'
 import { getProfile } from '$lib/services/ProfileService'
 
 export const load = (async ({ params }) => {
-  return await getProfile(params.username as string)
+  return await getProfile(params.username)
 }) satisfies PageLoad
